d14ee2e5d113de7d014a78f43ad1f1b88cf68149
Wemmons831 <69495081+Wemmons831@users.noreply.github.com>
2023-05-23 12:59:50 -0400

    added force

--- ball.hpp
+++ ball.hpp
@@ -5,13 +5,33 @@ class Ball
     int rad;
     Color color;
     Vector2 position;
-    Ball(Vector2 _position,int _rad, Color _color)
+    bool warp;
+    Vector2 force = {0,0}
+    Ball(Vector2 _position,int _rad, Color _color, _wrap = true)
     {
         rad = _rad;
         color = _color;
         position = _position;
+        wrap = _wrap;
     }
-    void Update(){}
+    void Update()
+    {
+        posistion = Vector2Add(posistion,force)
+        if(wrap){
+            if(posistion.x > 800){
+                posistion.x = 0;
+            }
+            if(posistion.x < 0){
+                posistion.x = 800
+            }
+            if(posistion.y > 450){
+                posistion.y = 0
+            }
+            if(posistion.y < 0){
+                posistion.y = 450
+            }
+        }
+    }
     void Draw()
     {
         DrawCircle(position.x,position.y,rad,color);
