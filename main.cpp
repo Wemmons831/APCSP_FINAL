2146469997e3b14430d2b268fb54a93777275f0c
Wemmons831 <69495081+Wemmons831@users.noreply.github.com>
2023-06-02 10:10:09 -0400

    test

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ int main(void)
     // home screen stuff
     int frame = 0;
     bool down = false;
-    Rectangle difficulty = {static_cast<float>(screenWidth/2 - 100),static_cast<float>(screenHeight + 50), 200, 75};
+    Rectangle difficulty = {static_cast<float>(screenWidth/2 - 100),screenHeight + 50, 200, 75};
     printf("X: %f, Y: ", difficulty.x,difficulty.y);
     char* currentDifficulty = "EASY";
 
