d81b61b8f59a8cfdc7267b5b07139ef14ab5a01f
Wemmons831 <69495081+Wemmons831@users.noreply.github.com>
2023-06-02 09:32:33 -0400

    start screen + orginization

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@ int main(void)
     //--------------------------------------------------------------------------------------
     const int screenWidth = 1280;
     const int screenHeight = 720;
+    int screen = 0;
+    int frame = 0
     Grid grid(5,5,{1270,710});
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
@@ -20,18 +22,32 @@ int main(void)
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
+        switch(screen)
+        {
+            case 0:
+                BeginDrawing();
+                    frame++
+                    if(frame > 20){
+                        frame = 0;
+                    }
+                    ClearBackground(RAYWHITE);
+                    DrawText("Color Code",GetScreenWidth()/2 - (MeasureText("Color Code",24 + frame)/2),GetScreenHeight()/2, 24+ frame, BLACK);
+                EndDrawing();
+                break;
+            case 1:
+                BeginDrawing();
+
+                    ClearBackground(GRAY);
+
+                    grid.Draw({0,0});
+                EndDrawing();
+                break;
+
+        }
        
         // Draw
         //----------------------------------------------------------------------------------
-        BeginDrawing();
-
-            ClearBackground(GRAY);
-            grid.Draw({0,0});
-        EndDrawing();
+       
         //----------------------------------------------------------------------------------
     }
 
